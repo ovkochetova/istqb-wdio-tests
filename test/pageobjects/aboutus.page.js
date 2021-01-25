f9c1a1cd2b777c580eae3b08c8a8e32a2031ec61
olga.kochetova <ovkochetova@gmail.com>
2021-01-25 10:28:30 +0300

    feat: adding lint

--- test/pageobjects/aboutus.page.js
+++ test/pageobjects/aboutus.page.js
@@ -1,25 +1,21 @@
 class AboutUsPage {
-
-    get AboutUsPage(){
-        const AboutUsPage = $('[href="/about-us.html"][class*="toggle"]');
+  get AboutUsPage() {
+    const AboutUsPage = $('[href="/about-us.html"][class*="toggle"]');
     return AboutUsPage;
-    }
+  }
 
-    get VisionMissionPage(){
-        const VisionMissionPage = $('[data-id="128"]>[href="/about-us/vision-mission.html"]');
+  get VisionMissionPage() {
+    const VisionMissionPage = $('[data-id="128"]>[href="/about-us/vision-mission.html"]');
     return VisionMissionPage;
-    }
-    
-    OpenAboutUs() {
-        this.AboutUsPage.click();
-    }
-    
-    OpenVisionMission(){
-        this.VisionMissionPage.click()
-    }
-    
-    
-    
+  }
+
+  OpenAboutUs() {
+    this.AboutUsPage.click();
+  }
+
+  OpenVisionMission() {
+    this.VisionMissionPage.click();
+  }
 }
 
-module.exports = new AboutUsPage()
+module.exports = new AboutUsPage();
